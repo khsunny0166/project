0db8a9c8058673a8bf3f571b1a09af5f8ad3e786
khsunny0166 <khsunny0166@gmail.com>
2026-03-10 21:50:12 +0900

    sw.js 업데이트

--- sw.js
+++ sw.js
@@ -1,7 +1,35 @@
-// Service Worker (설치 활성화를 위한 필수 파일)
-self.addEventListener('install', (e) => {
-  console.log('PWA Service Worker installed');
+const CACHE_NAME = 'junho-pwa-v1';
+const ASSETS = [
+  '/project/',
+  '/project/index.html',
+  '/project/manifest.json'
+];
+
+// 설치 단계: 파일들을 캐시에 저장
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  // 캐싱 로직 생략 가능
+
+// 활성화 단계: 이전 캐시 삭제
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 가져오기 단계: 캐시에서 파일 제공
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
