53079c382d1a8a073d0803f914bfeebdbebbee8a
khsunny0166 <khsunny0166@gmail.com>
2026-03-11 18:59:12 +0900

    service-worker.js 업데이트

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,18 @@
-const CACHE="workout-app-v1";
+const CACHE_NAME = 'junho-gym-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  // 여기에 캐싱할 주요 파일들을 추가하세요
+];
 
-self.addEventListener("install",e=>{
-
-self.skipWaiting();
-
+// 1. 설치 단계: 파일들 캐싱
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch",e=>{
-
+// 2. 요청 단계: 네트워크 대신 캐시에서 파일 찾아 제공
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
